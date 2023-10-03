46058af590d68ad47e731361cb63217229325951
Sachin <sachinlakshan04@gmail.com>
2023-10-04 00:25:29 +0530

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,15 @@ export default function Home() {
   return (
     <main className="">
       <section className="flex">
-        <div className="w-2/5">
-          <h1 className="text-8xl font-bold pt-20">Find your dream car</h1>
+        <div className="w-2/5 pt-20">
+          <p className="text-lg">
+            Browse, buy, and drive your dream car today.
+          </p>
+          <h1 className="text-8xl font-bold">Find your dream car</h1>
+          <p className="text-lg">
+            Join thousands of satisfied customers who've found their ideal car
+            here.
+          </p>
         </div>
         <Image
           className="w-3/5"
