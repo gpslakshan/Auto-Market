944d41ed3b912fe5324d2e52adb915b14b7f6dfb
Sachin <sachinlakshan04@gmail.com>
2023-10-04 17:56:28 +0530

    components created.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import NavBar from "@/components/NavBar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,9 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="w-full">
-      <section className="w-full flex flex-col xl:flex-row relative overflow-hidden">
-        <div className="xl:w-2/5 w-full pt-20">
-          <p className="text-lg">
-            Browse, buy, and drive your dream car today.
-          </p>
-          <h1 className="text-8xl font-bold">Find your dream car</h1>
-          <p className="text-lg">
-            Join thousands of satisfied customers who've found their ideal car
-            here.
-          </p>
-        </div>
-        <div className="xl:w-3/5 w-full flex justify-end items-end xl:h-screen">
-          <div className="relative w-full xl:h-full md:h-[560px] h-[400px] z-0">
-            {/* https://stackoverflow.com/questions/67421778/next-js-image-layout-fill-is-broken */}
-            <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          </div>
-          <div className="hero__image-overlay" />
-        </div>
-      </section>
+    <main className="w-full overflow-hidden">
+      <Hero />
     </main>
   );
 }
