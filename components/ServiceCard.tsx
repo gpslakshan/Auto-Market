32cdabeabdd3c66a94a3dce6b167e320f1f3711f
Sachin <sachinlakshan04@gmail.com>
2023-10-05 21:20:33 +0530

    mobile responsiveness for service card

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -13,8 +13,10 @@ const ServiceCard = ({ imgURL, label, subtext }: Props) => {
       <div className="w-11 h-11 flex justify-center items-center bg-green-400 rounded-full">
         <Image src={imgURL} alt={label} width={24} height={24} />
       </div>
-      <h3 className="mt-5 text-3xl leading-normal font-bold">{label}</h3>
-      <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray">
+      <h3 className="mt-5 text-lg sm:text-3xl leading-normal font-bold">
+        {label}
+      </h3>
+      <p className="mt-3 break-words font-montserrat text-sm sm:text-lg leading-normal text-slate-gray">
         {subtext}
       </p>
     </div>
